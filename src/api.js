7d9c0b6d0f791a36f4c9d2aa6fc631fdfadd3d46
akr0051 <akr0051@yahoo.com>
2021-08-31 11:40:39 -0500

    added linter

--- src/api.js
+++ src/api.js
@@ -1,24 +1,23 @@
-import { mockData } from './mock-data';
 import axios from 'axios';
 import NProgress from 'nprogress';
+import { mockData } from './mock-data';
 
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
+    var newurl = `${window.location.protocol
+    }//${
+      window.location.host
+    }${window.location.pathname}`;
+    window.history.pushState('', '', newurl);
   } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
+    newurl = `${window.location.protocol}//${window.location.host}`;
+    window.history.pushState('', '', newurl);
   }
 };
 
 const checkToken = async (accessToken) => {
   const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`,
   )
     .then((res) => res.json())
     .catch((error) => error.json());
@@ -27,15 +26,15 @@ const checkToken = async (accessToken) => {
 };
 
 const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
+  const extractLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractLocations)];
   return locations;
 };
 
 const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith("http://localhost")) {
+  if (window.location.href.startsWith('http://localhost')) {
     NProgress.done();
     return mockData;
   }
@@ -51,10 +50,10 @@ const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/get-events/' + token;
+    const url = `https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
-      var locations = extractLocations(result.data.events);
+      const locations = extractLocations(result.data.events);
       localStorage.setItem('lastEvents', JSON.stringify(result.data));
       localStorage.setItem('locations', JSON.stringify(locations));
     }
@@ -69,12 +68,12 @@ const getAccessToken = async () => {
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
   if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
+    await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
+    const code = await searchParams.get('code');
     if (!code) {
       const results = await axios.get(
-        "https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+        'https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url',
       );
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
@@ -82,22 +81,21 @@ const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
-
 };
 
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/token/' + encodeCode
+    `https://02ep2xk2eh.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`,
   )
-    .then((res) => {
-      return res.json();
-    })
+    .then((res) => res.json())
     .catch((error) => error);
 
-  access_token && localStorage.setItem("access_token", access_token);
+  access_token && localStorage.setItem('access_token', access_token);
 
   return access_token;
 };
 
-export { getEvents, getAccessToken, extractLocations, getToken, checkToken };
+export {
+  getEvents, getAccessToken, extractLocations, getToken, checkToken,
+};
